68bd87ecbb9a2895dc402341ebd2706b5bb5ea76
Joel Barnes <oelarnes@gmail.com>
2024-10-18 23:40:09 -0400

    more keybindings

--- init.lua
+++ init.lua
@@ -9,34 +9,34 @@
 --                          |_| \_|______\____/   \/   |_____|_|  |_|      |_|\_\______|  |_|  |_|  |_/_/    \_\_|     
 --                                                                                                                     
 --                                                                                       
---          lowercase                           uppercase                       CTRL-                           <leader>
---          =========                           =========                       =====                           ========
---  a       append                              append EOL                                                    
---  b       back word                           back WORD                       
---  c       change                              c$                              close buffer
---  d       delete                              delete EOL                      half-page down
---  e       end of word                         end of WORD                     load init.lua
---  f       find char                           ?find char                      explore cwd
---  g       go                                  goto line                       
---  h       left                                blank line above                window left                     turn off highlighting
---  i       insert                              insert BOL                      forward in jump stack       
---  j       down                                join lines                      window down
---  k       up                                  help lookup                     window up
---  l       right                               blank line below                
---  m       mark                                
---  n       next match                          previous match                  next buffer
---  o       insert below                        insert above                    back in jump stack
---  p       put                                 put before                      
---  q       record macro                        playback last register          quit
---  r       replace                             replace many                    redo
---  s       cl                                  c$                              save
---  t       to                                  back to                         
---  u       undo                                undo line                       half-page up
---  v       visual mode                         visual line                     visual block
---  w       word forward                        WORD forward                    window commands
---  x       delete char                         delete back                 
+--          lowercase                           uppercase                           CTRL-                           <leader>
+--          =========                           =========                           =====                           ========
+--  a       append                              append EOL                                                        
+--  b       back word                           back WORD                           
+--  c       change                              c$                                  close buffer
+--  d       delete                              delete EOL                          half-page down
+--  e       end of word                         end of WORD                         load init.lua
+--  f       find char                           ?find char                          explore cwd
+--  g       go                                  goto line                           
+--  h       left                                blank line above                    window left                     turn off highlighting
+--  i       insert                              insert BOL                          forward in jump stack       
+--  j       down                                join lines                          window down
+--  k       up                                  help lookup                         window up
+--  l       right                               blank line below                    window right                    ls
+--  m       mark                                                                 
+--  n       next match                          previous match                      next buffer                     toggle numbers
+--  o       insert below                        insert above                        back in jump stack
+--  p       put                                 put before                          
+--  q       record macro                        playback last register              quit
+--  r       replace                             replace many                        redo
+--  s       cl                                  c$                                  save
+--  t       to                                  back to                             
+--  u       undo                                undo line                           half-page up
+--  v       visual mode                         visual line                         visual block
+--  w       word forward                        WORD forward                        window commands
+--  x       delete char                         delete back                  
 --  y       yank                                yank lines                      
---  z
+--  z       folds                               ZZ/ZQ
 --  {[
 --  }]
 --  \|
@@ -56,6 +56,9 @@ vim.cmd.inoremap("jk <esc>")
 vim.cmd.colorscheme("elflord")
 vim.cmd.set("expandtab shiftwidth=4 tabstop=4")
 vim.cmd.set("number relativenumber")
+
+-- display
+vim.cmd.nnoremap("<leader>n :set number! relativenumber!<CR>")
 vim.cmd.nnoremap("<leader>h :noh<CR>")
 
 -- save and quit
@@ -82,6 +85,7 @@ vim.cmd.nnoremap("<C-j> <C-w>j")
 vim.cmd.nnoremap("<C-k> <C-w>k")
 vim.cmd.nnoremap("<C-l> <C-w>l")
 
+-- adding lines
 vim.cmd.nnoremap("L o<esc>")
 vim.cmd.nnoremap("H O<esc>")
 
@@ -89,3 +93,7 @@ vim.cmd.nnoremap("H O<esc>")
 vim.cmd.nnoremap("<C-c> :bd<CR>")
 vim.cmd.nnoremap("<C-n> :bn<CR>")
 vim.cmd.nnoremap("<C-p> :bp<CR>")
+
+-- commands
+vim.cmd.nnoremap("<leader>lb :ls<CR>")
+vim.cmd.nnoremap("<leader>ld :!ls<CR>")
