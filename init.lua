dc0c4b55f994542c7bf7e3b8f273caad3d616030
Joel Barnes <oelarnes@gmail.com>
2024-10-18 23:25:20 -0400

    more init

--- init.lua
+++ init.lua
@@ -13,21 +13,21 @@
 --          =========                           =========                       =====                           ========
 --  a       append                              append EOL                                                    
 --  b       back word                           back WORD                       
---  c       change                              c$                              
+--  c       change                              c$                              close buffer
 --  d       delete                              delete EOL                      half-page down
 --  e       end of word                         end of WORD                     load init.lua
 --  f       find char                           ?find char                      explore cwd
---  g
---  h
---  i
---  j
---  k
---  l
---  m
---  n
---  o
---  p
---  q
+--  g       go                                  goto line                       
+--  h       left                                blank line above                window left                     turn off highlighting
+--  i       insert                              insert BOL                      forward in jump stack       
+--  j       down                                join lines                      window down
+--  k       up                                  help lookup                     window up
+--  l       right                               blank line below                
+--  m       mark                                
+--  n       next match                          previous match                  next buffer
+--  o       insert below                        insert above                    back in jump stack
+--  p       put                                 put before                      
+--  q       record macro                        playback last register          quit
 --  r
 --  s
 --  t
@@ -56,19 +56,36 @@ vim.cmd.inoremap("jk <esc>")
 vim.cmd.colorscheme("elflord")
 vim.cmd.set("expandtab shiftwidth=4 tabstop=4")
 vim.cmd.set("number relativenumber")
+vim.cmd.nnoremap("<leader>h :noh<CR>")
 
--- save
+-- save and quit
 vim.cmd.inoremap("<C-s> <esc>:w<CR>i")
 vim.cmd.nnoremap("<C-s> :w<CR>")
+vim.cmd.nnoremap("<C-q> :q<CR>")
 
 -- centering
 vim.cmd.nnoremap("<C-d> <C-d>zz")
 vim.cmd.nnoremap("<C-u> <C-u>zz")
 vim.cmd.nnoremap("n nzz")
 vim.cmd.nnoremap("N Nzz")
+vim.cmd.nnoremap("G Gzz")
 
 -- file loading
 vim.cmd.nnoremap("<C-e> :e ~/.config/nvim/init.lua<CR>")
 
 -- explorer
 vim.cmd.nnoremap("<C-f> :Explore $PWD<CR>")
+
+-- window
+vim.cmd.nnoremap("<C-h> <C-w>h")
+vim.cmd.nnoremap("<C-j> <C-w>j")
+vim.cmd.nnoremap("<C-k> <C-w>k")
+vim.cmd.nnoremap("<C-l> <C-w>l")
+
+vim.cmd.nnoremap("L o<esc>")
+vim.cmd.nnoremap("H O<esc>")
+
+-- buffers
+vim.cmd.nnoremap("<C-c> :bd<CR>")
+vim.cmd.nnoremap("<C-n> :bn<CR>")
+vim.cmd.nnoremap("<C-p> :bp<CR>")
